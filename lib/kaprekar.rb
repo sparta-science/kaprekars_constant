6c0cc05230559cb120556b18a7afecf258f45515
Harold Tafur <harold@tafurs.com>
2020-01-29 12:09:15 -0800

    The solution
    Co-authored-by: Diana Chou <dschou10@gmail.com>

--- lib/kaprekar.rb
+++ lib/kaprekar.rb
@@ -1,5 +1,26 @@
 class Kaprekar
-  def steps_to_kaprekar
-    # Something in here
+  KAPREKAR_CONSTANT = 6174
+
+  def self.steps_to_kaprekar(number)
+    raise "Input not long enough" if self.is_number_invalid?(number)
+    steps = 0
+    current_number = number
+
+    while KAPREKAR_CONSTANT != current_number do
+      number_as_string = current_number.to_s
+      number_as_string_length = number_as_string.length
+      (4 - number_as_string_length).times { |i| number_as_string << "0" }
+
+      number_asc = number_as_string.chars.sort
+      number_desc = number_asc.reverse
+      current_number = number_desc.join.to_i - number_asc.join.to_i
+      steps += 1
+    end
+
+    steps
+  end
+
+  def self.is_number_invalid?(number)
+    number.to_s.length < 4
   end
 end
--- spec/kaprekar_spec.rb
+++ spec/kaprekar_spec.rb
@@ -1,17 +1,21 @@
 require 'kaprekar'
 
 RSpec.describe Kaprekar do
-  describe 'some test' do
-    it 'does something' do
-      # 6174 -> 0
+  describe 'steps_to_kaprekar' do
+    it "returns 0, if it receives kaprekar's constant" do
+      expect(Kaprekar.steps_to_kaprekar(6174)).to eq(0)
     end
 
-    it 'does something' do
-      # 6589 -> 2
+    it 'returns 2, if it receives 6589' do
+      expect(Kaprekar.steps_to_kaprekar(6589)).to eq(2)
     end
 
-    it 'does something' do
-      # 5455 -> 5
+    it 'returns 5, if it receives 5455' do
+      expect(Kaprekar.steps_to_kaprekar(5455)).to eq(5)
+    end
+
+    it 'raises an exception if number is not 4 digits' do
+      expect{ Kaprekar.steps_to_kaprekar(545) }.to raise_exception("Input not long enough")
     end
   end
 end
