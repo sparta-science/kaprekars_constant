d6a63685c3d4ee26bdb5dbea3d078ea1c6a0294e
Diana Chou <dschou10@gmail.com>
2020-03-11 16:02:21 -0700

    paul

--- lib/kaprekar.rb
+++ lib/kaprekar.rb
@@ -1,5 +1,21 @@
 class Kaprekar
-  def steps_to_kaprekar
-    # Something in here
+  def self.steps_to_kaprekar(number)
+    steps =0
+    result = number
+    while result != 6174
+      descending = sort_digits(result, false )
+      ascending = sort_digits(result, true )
+      result = descending - ascending
+      steps += 1
+    end
+    steps
+  end
+
+  def self.sort_digits(number, ascending)
+    sorted = number.to_s.chars.sort
+    if !ascending
+      sorted = sorted.reverse
+    end
+    sorted.join.to_i
   end
 end
--- spec/kaprekar_spec.rb
+++ spec/kaprekar_spec.rb
@@ -1,17 +1,39 @@
 require 'kaprekar'
 
 RSpec.describe Kaprekar do
-  describe 'some test' do
-    it 'does something' do
-      # 6174 -> 0
+  describe 'Kaprekar steps_to_kaprekar' do
+    context 'Kaprekar' do
+      it 'returns 0' do
+        expect(Kaprekar.steps_to_kaprekar(6174)).to eq(0)
+      end
     end
 
-    it 'does something' do
-      # 6589 -> 2
+    context '6589' do
+      it 'returns 2' do
+        expect(Kaprekar.steps_to_kaprekar(6589)).to eq(2)
+      end
     end
 
-    it 'does something' do
-      # 5455 -> 5
+    context '5455' do
+      it 'returns 5' do
+        expect(Kaprekar.steps_to_kaprekar(5455)).to eq(5)
+      end
+    end
+  end
+
+  describe 'Kaprekar sort_digits' do
+    context 'Kaprekar' do
+      context 'ascending' do
+        it 'returns 1467' do
+          expect(Kaprekar.sort_digits(6174, true)).to eq(1467)
+        end
+      end
+      context 'descending' do
+        it 'returns 7641' do
+          expect(Kaprekar.sort_digits(6174, false)).to eq(7641)
+        end
+      end
+
     end
   end
 end
